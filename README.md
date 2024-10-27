5f7da3ebcb2480d1b9f30959a0f1749ccf1a34c6
Crapper <crapper.612@gmail.com>
2024-10-28 00:29:59 +0800

    Change to interactive node

--- README.md
+++ README.md
@@ -5,4 +5,16 @@ wget https://github.com/25077667/standalone-python/releases/download/release-202
 
 # unzip
 tar -xvf release-3.10-x86_64.tar.gz
+
+# setup venv
+./build_venv.sh
+
+# Chmod
+chmod 777 ./venv/bin/activate
+
+# Activate Env
+. ./venv/bin/activate 
+
+# exit env when activate
+deactivate
 ```
--- build_venv.sh
+++ build_venv.sh
@@ -1,14 +1,13 @@
 ./opt/python/bin/python -m venv venv
-
 source venv/bin/activate
-
 pip list
 pip install --upgrade pip
 pip install --upgrade git+https://github.com/huggingface/transformers
 pip install requests
-pip install triton
+# pip install triton
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu113
 pip install pillow
 
+pip list
 
 # pip install 
