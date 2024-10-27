eaf252f25dc2fdf292f5821f18d6e4cb0aaa210d
Crapper <crapper.612@gmail.com>
2024-10-28 01:19:44 +0800

    Add time counter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from PIL import Image
+import time
 os.environ["HF_TOKEN"] = "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"
 
 from transformers import AutoProcessor, AutoModelForPreTraining
@@ -8,6 +9,7 @@ from transformers import AutoProcessor, AutoModelForPreTraining
 processor = AutoProcessor.from_pretrained("unsloth/Llama-3.2-11B-Vision-Instruct-bnb-4bit")
 model = AutoModelForPreTraining.from_pretrained("unsloth/Llama-3.2-11B-Vision-Instruct-bnb-4bit")
 
+t = time.time()
 url = "https://img-9gag-fun.9cache.com/photo/aPAYX5Q_460s.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 #Return JSON format of {object:array, body_lang:array, emotion:array, tone:array, text:string}
@@ -16,3 +18,4 @@ inputs = processor(image, prompt, return_tensors="pt").to(model.device)
 
 output = model.generate(**inputs, max_new_tokens=200)
 print(processor.decode(output[0]).replace(prompt, ""))
+print("Time used: ", time.time() - t)
