228a365d61ed53d12b8cb6341cf4ed9ddf871efc
Crapper <crapper.612@gmail.com>
2024-10-28 01:19:32 +0800

    Modify package installation shell script

--- build_venv.sh
+++ build_venv.sh
@@ -1,12 +1,13 @@
-./opt/python/bin/python -m venv venv
+python -m venv venv
 source venv/bin/activate
 pip list
 pip install --upgrade pip
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip install --upgrade git+https://github.com/huggingface/transformers
-pip install requests
-# pip install triton
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu113
 pip install pillow
+pip install requests
+pip install bitsandbytes
+pip install 'accelerate>=0.26.0'
 
 pip list
 
