39ceff82ae49a99a1a9d622dc5f3575343b38fda
Joe Lum <joelum-hk@proton.me>
2024-11-05 19:23:25 +0800

    Work in progress intern_vl_2

--- steps/intern_vl_2_image_meaning/README.md
+++ steps/intern_vl_2_image_meaning/README.md
@@ -22,8 +22,31 @@ nvidia-smi
 
 # Run the script
 export CUDA_VISIBLE_DEVICES="1"
-python ./main.py
+python ./main.py 1 0
 
 # Read the last 10 posts
 python ./tail.py
+```
+
+## Run with multiple workers in the background
+
+```sh
+cd ./steps/llama3_2_describe_the_image
+
+# See available GPUs
+nvidia-smi
+
+# IMPORTANT: Set the CUDA_VISIBLE_DEVICES environment variable to the GPU you want to use
+screen -dmS memes_processor1 bash -c 'export CUDA_VISIBLE_DEVICES="1" && source ./venv/bin/activate && python main.py 2 0'
+
+screen -dmS memes_processor2 bash -c 'export CUDA_VISIBLE_DEVICES="2" && source ./venv/bin/activate && python main.py 2 1'
+
+# check if screen is running
+screen -ls
+
+# attach to screen
+screen -r memes_processor1
+
+# detach from screen
+Ctrl + A + D
 ```
--- steps/intern_vl_2_image_meaning/main.py
+++ steps/intern_vl_2_image_meaning/main.py
@@ -5,12 +5,22 @@ import logging
 import pandas as pd
 import os
 import requests
+import sys
 from queue import Queue
 
 from PIL import Image
 from core.data_storage import DataPost, DataString, append_post, read_data_grid_ids
 from lmdeploy import pipeline, TurbomindEngineConfig
-from lmdeploy.vl import load_image
+
+
+num_workers = 1
+worker_id = 0
+if len(sys.argv) != 3:
+    print("Usage: python main.py <num_workers> <worker_id>")
+    sys.exit(1)
+else:
+    num_workers = int(sys.argv[1])
+    worker_id = int(sys.argv[2])
 
 
 logging.basicConfig(level=logging.INFO)
@@ -28,15 +38,6 @@ def read_tsv(tsv_file_path: str) -> pd.DataFrame:
         )
 
 
-stage1_df = read_tsv("../../results/9gag-memes-dataset-stage1-10k.tsv")
-
-
-if os.path.exists("9gag-memes-intern-image-meaning.bin"):
-    data_ids = read_data_grid_ids("9gag-memes-intern-image-meaning.bin")
-else:
-    data_ids = []
-
-
 def safe_string(value: str) -> str:
     """
     Decode the string by removing the last byte until it is valid.
@@ -50,6 +51,20 @@ def safe_string(value: str) -> str:
     return ""
 
 
+def log_prefix():
+    return f"[{time.strftime('%H:%M:%S', time.localtime(time.time()))}][Worker {worker_id}]"
+
+
+logger.info(f"{log_prefix()} Reading tsv file...")
+stage1_df = read_tsv("../../results/9gag-memes-dataset-stage1-10k.tsv")
+
+logger.info(f"{log_prefix()} Checking if data grid exists...")
+if os.path.exists("9gag-memes-intern-image-meaning.bin"):
+    data_ids = read_data_grid_ids("9gag-memes-intern-image-meaning.bin")
+else:
+    data_ids = []
+
+logger.info(f"{log_prefix()} Initializing model...")
 queue_size = 10
 
 model = "OpenGVLab/InternVL2-8B"
@@ -71,17 +86,24 @@ class Producer:
         self.q = q
 
     def produce(self):
-        j = 0
+        total = len(self.df)
         for index, row in self.df.iterrows():
+            if index % num_workers != worker_id:
+                continue
+
             while self.q.full():
                 time.sleep(1)
 
             id = row["id"]
             image_url = row["image_url"]
             if id in data_ids:
-                logger.info(f"Skipping {id} because it is already processed")
+                logger.info(
+                    f"{log_prefix()} Skipping {id} because it is already processed ({index} of {total})"
+                )
             elif image_url == "<video-content>":
-                logger.info(f"Skipping {id} because it is a video")
+                logger.info(
+                    f"{log_prefix()} Skipping {id} because it is a video ({index} of {total})"
+                )
             else:
                 try:
                     image = Image.open(requests.get(image_url, stream=True).raw)
@@ -90,16 +112,13 @@ class Producer:
                     comments = row["comments"]
                     self.q.put(ImagePost(id, title, image, upvotes, comments))
 
-                    logger.info(
-                        f"[{time.strftime('%H:%M:%S', time.localtime(time.time()))}] Fetched {j}th image post"
-                    )
+                    logger.info(f"{log_prefix()} Fetched {id} image post ({index} of {total})")
                 except KeyboardInterrupt:
                     break
                 except Exception as e:
-                    logger.error(f"Error reading image {image_url}: {e}")
-                    continue
+                    logger.error(f"{log_prefix()} Error reading image {image_url}: {e}")
 
-        logger.info("Producer finished")
+        logger.info(f"{log_prefix()} Producer finished")
 
 
 class Consumer:
@@ -114,9 +133,7 @@ class Consumer:
 
             try:
                 start_time = time.time()
-                logger.info(
-                    f"[{time.strftime('%H:%M:%S', time.localtime(start_time))}] Processing {j}th post"
-                )
+                logger.info(f"{log_prefix()} Processing {j}th post")
 
                 post: ImagePost = self.q.get()
                 prompt = "what is the meaning of this image"
@@ -135,15 +152,17 @@ class Consumer:
                     ),
                 )
 
-                logger.info(f"Processed {post.id} in {time.time() - start_time:.2f} seconds")
+                logger.info(
+                    f"{log_prefix()} Processed {post.id} in {time.time() - start_time:.2f} seconds"
+                )
             except KeyboardInterrupt:
                 break
             except Exception as e:
-                logger.error(e)
+                logger.error(f"{log_prefix()} Error processing post: {e}")
 
             j += 1
 
-        logger.info("Consumer finished")
+        logger.info(f"{log_prefix()} Consumer finished")
 
 
 image_queue = Queue(queue_size)
@@ -161,23 +180,4 @@ consumer_thread.start()
 producer_thread.join()
 consumer_thread.join()
 
-logger.info("All threads finished")
-
-# current_folder = pathlib.Path(__file__).parent
-# images_folder = current_folder / "images"
-
-# start_time = time.time()
-# for image_path in images_folder.iterdir():
-#     path = image_path.resolve().as_posix()
-#     image = load_image(path)
-#     response = pipe(("what is the meaning of this image", image))
-#     print(response.text)
-#     with open(f"meaning_intern_vl_2/{image_path.stem}.txt", "w") as f:
-#         f.write(response.text)
-
-# end_time = time.time()
-
-# elapsed_time = end_time - start_time
-# print(f"Time taken: {elapsed_time:.2f} seconds")
-
-# Time taken: 8.44 seconds
+logger.info(f"{log_prefix()} All threads finished")
